4c638a61f4532e1a29a537e39ecba43cad19056c
farrel dinarta <farrel.dinarta@student.umn.ac.id>
2023-11-29 00:44:58 +0700

    add : name from auth data on reservation screen

--- app/src/main/java/com/example/mobile_fsd_jetpack/api/BaseAPIBuilder.kt
+++ app/src/main/java/com/example/mobile_fsd_jetpack/api/BaseAPIBuilder.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.scalars.ScalarsConverterFactory
 class BaseAPIBuilder {
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl("https://f0e0-111-67-81-50.ngrok-free.app/api/")
+            .baseUrl("https://498d-2001-448a-2042-3e13-b0c7-ad8e-227f-3bb3.ngrok-free.app/api/")
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
